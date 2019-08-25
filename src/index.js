c0b8b58d3e5aeac26a38d491c114a78acab8bcb9
onecastell <bloofrank55@gmail.com>
2019-08-25 01:42:33 -0400

    chore: enhance graph and add new server entries

--- src/index.js
+++ src/index.js
@@ -4,34 +4,48 @@ import ForceGraph3D from '3d-force-graph'
 const servers = [
     { id: 'facebook', uri: 'https://www.facebook.com/platform/api-status/' },
     { id: 'twitter', uri: 'https://api.twitterstat.us/api/v2/status.json' },
-    // {id: '', uri: ''},
+    { id: 'github', uri: 'https://www.githubstatus.com/api/v2/status.json' },
+    { id: 'reddit', uri: 'https://reddit.statuspage.io/api/v2/status.json' },
+    { id: 'dropbox', uri: 'https://status.dropbox.com/api/v2/status.json' },
+    // { id: 'reddit', uri: '' },
 ]
 
+
 const dataSource = {
-    //  Define nodes array
+    //  Define nodes
     nodes: [...servers.keys()].map(n => ({ id: n, group: 2 })),
-    //  Define links array
+    //  Define links 
     links: [...servers.keys()].map(id => ({ source: id, target: 0 }))
 }
 
 const pointsGraph = ForceGraph3D()
-    (document.body).graphData(dataSource)
+    (document.body)
+    .backgroundColor('#f5f5f5')
+    .showNavInfo(false)
+    .cameraPosition({ z: 100 }, null, 500)
+    .linkDirectionalParticles(d => Math.floor(Math.random() * 7))
+    .linkDirectionalParticleSpeed(.005)
+    .graphData(dataSource)
 
 // Ping each server
-for (let server of servers) {
+for (let [index, server] of servers.entries()) {
+    // console.log(server, index)
     fetch('https://cors-anywhere.herokuapp.com/'.concat(server.uri))
         .then(r => r.json())
         .then(d => {
             switch (server.id) {
                 case 'facebook':
                     d.current.health === 1
-                        ? dataSource.nodes[0].color = '#0f0'
-                        : dataSource.nodes[0].color = '#f00'
+                        ? dataSource.nodes[index].color = '#0f0'
+                        : dataSource.nodes[index].color = '#f00'
                     break
+                case 'dropbox':
+                case 'github':
+                case 'reddit':
                 case 'twitter':
                     d.status.indicator === 'none'
-                        ? dataSource.nodes[1].color = '#0f0'
-                        : dataSource.nodes[1].color = '#f00'
+                        ? dataSource.nodes[index].color = '#0f0'
+                        : dataSource.nodes[index].color = '#f00'
                     break
             }
             pointsGraph.nodeRelSize(4) // Update Graph
