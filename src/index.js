1d879a4cdbd633b8d4709c729471b39a1acf8621
onecastell <bloofrank55@gmail.com>
2019-08-29 21:39:33 -0400

    feat: add font-awesome icons for each menu item

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import ForceGraph3D from '3d-force-graph'
 import SpriteText from 'three-spritetext'
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fab)
+dom.i2svg()
+dom.watch()
+
 // API Server entries
 const servers = [
-    { label: 'facebook', uri: 'https://www.facebook.com/platform/api-status/' },
-    { label: 'twitter', uri: 'https://api.twitterstat.us/api/v2/status.json' },
-    { label: 'github', uri: 'https://www.githubstatus.com/api/v2/status.json' },
-    { label: 'reddit', uri: 'https://reddit.statuspage.io/api/v2/status.json' },
-    { label: 'dropbox', uri: 'https://status.dropbox.com/api/v2/status.json' },
+    { label: 'facebook', uri: 'https://www.facebook.com/platform/api-status/', color: '#1877f2' },
+    { label: 'twitter', uri: 'https://api.twitterstat.us/api/v2/status.json', color: '#1da1f2' },
+    { label: 'github', uri: 'https://www.githubstatus.com/api/v2/status.json', color: '#222222' },
+    { label: 'reddit', uri: 'https://reddit.statuspage.io/api/v2/status.json', color: '#ff4500' },
+    { label: 'dropbox', uri: 'https://status.dropbox.com/api/v2/status.json', color: '#0061FF' },
 ]
 
 const dataSource = {
@@ -22,8 +28,8 @@ const graph = ForceGraph3D()
     .showNavInfo(false)
     .cameraPosition({ z: 100 }, null, 500)
     .nodeThreeObjectExtend(true)
-    .nodeThreeObject(node=>{
-        const text = new SpriteText(node.label)
+    .nodeThreeObject(node => {
+        const text = new SpriteText(String(node.label).toUpperCase())
         text.color = '#15151599'
         text.fontFace = 'Teko'
         text.textHeight = 3.5
@@ -35,6 +41,7 @@ const graph = ForceGraph3D()
     .linkDirectionalParticleSpeed(.005)
     .graphData(dataSource)
 
+// Rotate Camera
 // setTimeout(() => {
 //     let angle = 0;
 //     setInterval(() => {
@@ -76,17 +83,23 @@ const overlay = document.createElement('main')
 document.body.appendChild(overlay)
 
 // Populate Overlay
-for (let [index,server] of servers.entries()){
+for (let [index, server] of servers.entries()) {
     const button = document.createElement('button')
     button.title = server.label
-    button.addEventListener('mouseover',event=>{
+    button.style.color = server.color
+
+    const icon = document.createElement('i')
+    icon.className = 'fab fa-' + server.label
+    icon.style = "filter:drop-shadow(0 0 5px " + server.color + ")"
+
+    button.appendChild(icon)
+    button.addEventListener('click', event => {
         const node = dataSource.nodes[index]
-        console.log(node)
         graph.cameraPosition({
             x: node.x,
             y: node.y,
             z: node.z + 50
-        },node,2000);
+        }, node, 2000);
     })
     overlay.appendChild(button)
 }
